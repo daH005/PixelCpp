5bf7f2d0cac84100e23b166fa8bd53bc3817a225
Danil Shevelev <danil.shevelev.2004@mail.ru>
2026-01-25 19:42:05 +0500

    build.sh updated

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-g++ main.cpp -o PixelCpp -I ../SFML-2.6.2/include/ -L ../SFML-2.6.2/lib/ -lsfml-graphics -lsfml-window -lsfml-system
+# 1. Add SFML-2.6.2/include/SFML into /usr/include
+# 2. Add SFML-2.6.2/lib into /usr/lib
+# 3. Run:
+g++ main.cpp -o PixelCpp -lsfml-graphics -lsfml-window -lsfml-system
